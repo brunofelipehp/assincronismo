08ec63990b727d1a2e1b881f27d3163614394772
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-07 12:51:01 -0300

    :tada: Aprendendo sobre Promise

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Promise
+ * 
+ * A promessa de que algo irá acontecer
+ * Poderá dar certo ou errado.
+ * mas irá acontecer
+ */
+
+let aceitar = true
+
+console.log('pedir uber');
+const promessa = new Promise((resolve, reject) => {
+
+    if (aceitar) {
+        return resolve('pedido aceito')
+    }
+
+    return reject('pedido negado!')
+})
+
+console.log('aguardando');
+
+promessa
+.then((result) => console.log(result))
+.catch(erro => console.log(erro))
+.finally(() => console.log('finalizado'))
---
+++ setTimeout.js
@@ -0,0 +1,5 @@
+// setTimeout(function, delay) 
+
+setTimeout(function(){
+    console.log('depois de 2s');
+}, 2000)
